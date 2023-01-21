78215aa83ff71739b9332b8ef08face24b0b69e8
RafalM2000 <rfm@europe.com>
2023-01-21 21:15:26 +0100

    zod instalation

--- src/app/componensts/books/books.component.ts
+++ src/app/componensts/books/books.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ColDef } from 'ag-grid-community';
 
 @Component({
   selector: 'app-books',
@@ -12,17 +11,4 @@ export class BooksComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  columnDefs: ColDef[] = [
-    { field: 'title' },
-    { field: 'author' },
-    { field: 'year' },
-    { field: 'rating' }
-  ];
-
-  rowData = [
-    { title: 'Idiota', author: 'Fiodor Dostojewski', year: 1869, rating: '****' },
-    { title: 'Salambo', author: 'Gustave Flaubert', year: 1862, rating: '*****' },
-    { title: 'Jądro ciemności', author: 'Konrad Korzeniowski', year: 1899, rating: '*****' }
-];
-
 }
